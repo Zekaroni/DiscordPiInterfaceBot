1f621aef65ffe75f8069639418a16dc09cf6c8a6
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-06-13 13:25:03 -0400

    reformatted code

---
+++ lib/bot.py
@@ -0,0 +1,41 @@
+from discord import Intents
+from discord.ext import commands
+from lib.settings import BotSettings
+from lib.utilcmds import Device
+
+raspPi = Device()
+
+class DiscordBot:
+    def __init__(self) -> None:
+        self.local_settings = BotSettings()
+        self._intents = Intents.all()
+        self.bot = commands.Bot(command_prefix='>', intents=self._intents)
+
+        @self.bot.command()
+        async def ping(ctx):
+            await ctx.channel.send('Server is running.')
+
+        @self.bot.command()
+        async def setup(ctx):
+            try:
+                pin_num, output = ctx.message.content.split(" ")
+                raspPi.setup_pin((int(pin_num)),bool(output.capitalize()))
+            except TypeError:
+                ctx.channel.send("Invalid argument")
+
+        # @self.botbot.command()
+        # async def on(ctx):
+        #     GPIO.output(led_pin, GPIO.HIGH)
+        #     await ctx.channel.send('LED turned on.')
+
+        # @self.bot.command()
+        # async def off(ctx):
+        #     GPIO.output(led_pin, GPIO.LOW)
+        #     await ctx.channel.send('LED turned off.')
+
+        @self.bot.command()
+        async def temp(ctx):
+            await ctx.channel.send(f"CPU temperature is : {raspPi.get_cpu_temperature()}C\nGPU temperature is : {raspPi.get_gpu_temperature()}C")
+
+    def start(self):
+        self.bot.run(self.local_settings.key)
---
+++ lib/utilcmds.py
@@ -0,0 +1,47 @@
+import subprocess
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+
+
+class Device:
+    def __init__(self):
+        self._active_pins = [] # For pins that get activated
+
+        self._banned_pins = [
+            3,    # Pull-up resistor, input
+            5,    # Pull-up resistor, input
+            13,   # Reserved
+            19,   # Reserved
+            21,   # Reserved
+            23,   # Reserved
+            24,   # Reserved
+            27,   # ID EEPROM
+            28,   # ID EEPROM
+            29,   # ID EEPROM
+            31,   # Reserved
+            33,   # Reserved
+            35,   # Reserved
+            37,   # Reserved
+            38,   # Reserved
+            40,   # Reserved
+        ]
+
+    def setup_pin(self, pin_num, pin_out) -> None:
+        """
+        param pin_num: The number of the pin on the board
+        param pin_out: True if you want pin output, False for pin input
+        """
+        if pin_num not in self._banned_pins and pin_num not in self._active_pins:
+            GPIO.setup(pin_num, GPIO.OUT if pin_out is True else GPIO.IN)
+            self._active_pins.append(pin_num)
+
+    @staticmethod
+    def get_cpu_temperature() -> float:
+        with open('/sys/class/thermal/thermal_zone0/temp', 'r') as f:
+            temp = round(int(f.read().strip()) / 1000.0,1)
+        return temp
+
+    @staticmethod
+    def get_gpu_temperature() -> str:
+        return subprocess.check_output("vcgencmd measure_temp", shell=True).decode("utf-8").replace('temp=','').replace("'C\n",'')
