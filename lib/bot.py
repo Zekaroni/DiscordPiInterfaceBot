b06e9ee9bcdee6c283229d2a85f23f57d74be9d8
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-06-13 15:59:06 -0400

    integrating slash commands

--- lib/bot.py
+++ lib/bot.py
@@ -2,40 +2,57 @@ from discord import Intents
 from discord.ext import commands
 from lib.settings import BotSettings
 from lib.utilcmds import Device
+import interactions
 
 raspPi = Device()
+
+active_guilds = [
+    949408545709908059,
+]
 
 class DiscordBot:
     def __init__(self) -> None:
         self.local_settings = BotSettings()
         self._intents = Intents.all()
-        self.bot = commands.Bot(command_prefix='>', intents=self._intents)
+        self.bot = interactions.Client(token=BotSettings().key)
 
-        @self.bot.command()
-        async def ping(ctx):
-            await ctx.channel.send('Server is running.')
+        @self.bot.command(
+            name="say_something",
+            description="say something!",
+            scope=active_guilds,
+            options = [
+                interactions.Option(
+                    name="pinNumber",
+                    description="What you want to say",
+                    type=interactions.OptionType.NUMBER,
+                    required=True,
+                ),
+            ],
+        )
+        async def ping(ctx:interactions.CommandContext, pinNumber: float):
+            await ctx.channel.send(f'{pinNumber} : {type(pinNumber)}')
 
-        @self.bot.command()
-        async def setup(ctx):
-            try:
-                pin_num, output = ctx.message.content.split(" ")[1:]
-                raspPi.setup_pin((int(pin_num)),bool(output.capitalize()))
-                # TODO: Allow for true|false and in|out
-                await ctx.channel.send(f"Pin {pin_num} was set up.")
-            except Exception as e:
-                if e == TypeError:
-                    await ctx.channel.send("Invalid agrument use `>setup <enter pin num 1-40 here> <true|false>`")
-                else:
-                    await ctx.channel.send(e)
+        # @self.bot.command()
+        # async def setup(ctx):
+        #     try:
+        #         pin_num, output = ctx.message.content.split(" ")[1:]
+        #         raspPi.setup_pin((int(pin_num)),bool(output.capitalize()))
+        #         # TODO: Allow for true|false and in|out
+        #         await ctx.channel.send(f"Pin {pin_num} was set up.")
+        #     except Exception as e:
+        #         if e == TypeError:
+        #             await ctx.channel.send("Invalid agrument use `>setup <enter pin num 1-40 here> <true|false>`")
+        #         else:
+        #             await ctx.channel.send(e)
 
-        @self.bot.command()
-        async def enable(ctx):
-            raspPi.setup_pin
-            await ctx.channel.send('LED turned on.')
+        # @self.bot.command()
+        # async def enable(ctx):
+        #     raspPi.setup_pin
+        #     await ctx.channel.send('LED turned on.')
 
-        @self.bot.command()
-        async def temp(ctx):
-            await ctx.channel.send(f"CPU temperature is : {raspPi.get_cpu_temperature()}C\nGPU temperature is : {raspPi.get_gpu_temperature()}C")
+        # @self.bot.command()
+        # async def temp(ctx):
+        #     await ctx.channel.send(f"CPU temperature is : {raspPi.get_cpu_temperature()}C\nGPU temperature is : {raspPi.get_gpu_temperature()}C")
 
     def start(self):
-        self.bot.run(self.local_settings.key)
+        self.bot.start()
