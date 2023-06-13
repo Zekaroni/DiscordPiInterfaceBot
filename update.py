4da92a8e0578e7467ed9747ffa09e3c5b60c8452
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-06-13 12:20:36 -0400

    changed update to sh file

--- update.py
+++
@@ -1,3 +0,0 @@
-import os
-os.system('cd ..')
-os.system('git clone https://github.com/Zekaroni/FilesForPi.git')
---
+++ update.sh
@@ -0,0 +1,4 @@
+cd ..
+rm -rf FilesForPi
+git clone https://github.com/Zekaroni/FilesForPi.git
+cd FilesForPi
