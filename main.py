4306d0a982a8cd90e97682854f66a6cc7ec57fb3
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-06-15 17:21:59 -0400

    okay, actual last try

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from lib.bot import DiscordBot
 
+exec(open('./.venv/bin/activate_this.py').read(), {'__file__': './.venv/bin/activate_this.py'}) # Activates venv
 hostedBot = DiscordBot()
 hostedBot.start()
