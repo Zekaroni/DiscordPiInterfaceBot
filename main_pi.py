632d913321aa09630e8a802273572b17c2462c4d
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-07-01 18:48:44 -0400

    changed serial out for pi 3 model b

--- main_pi.py
+++ main_pi.py
@@ -1,7 +1,7 @@
 from serial import Serial
 
 # Create a serial object with the appropriate port and baud rate settings
-ser = Serial('/dev/ttyS0', 9600)
+ser = Serial('/dev/serial0', 9600)
 
 # Send a byte of data to the Arduino
 data_byte = 0x41  # Byte value to send
