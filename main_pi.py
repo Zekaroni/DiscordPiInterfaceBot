fe25707609d429e3f908fa8bd88cde0f1ff58067
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-07-01 19:02:35 -0400

    trying ddifferent formatting

--- main_pi.py
+++ main_pi.py
@@ -1,16 +1,14 @@
-from serial import Serial
+import serial
 
 try:
     # Create a serial object with the appropriate port and baud rate settings
-    ser = Serial('/dev/serial1', 9600)
+    ser = serial.Serial('/dev/serial1', 9600)
 
     # Send a byte of data to the Arduino
-    data_byte = 0x41  # Byte value to send
+    data_byte = b'\x41'  # Byte value to send
     ser.write(data_byte)
 
     # Close the serial connection
     ser.close()
 except Exception as e:
     print("Serial communication error:", str(e))
-
-    
