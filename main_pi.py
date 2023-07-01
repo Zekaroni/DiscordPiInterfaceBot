00a3201c48c48b8007f97f6ab8fe34d932e0f7cb
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-07-01 18:53:40 -0400

    added debug lines

--- main_pi.py
+++ main_pi.py
@@ -1,11 +1,16 @@
 from serial import Serial
 
-# Create a serial object with the appropriate port and baud rate settings
-ser = Serial('/dev/serial1', 9600)
+try:
+    # Create a serial object with the appropriate port and baud rate settings
+    ser = Serial('/dev/serial1', 9600)
 
-# Send a byte of data to the Arduino
-data_byte = 0x41  # Byte value to send
-ser.write(data_byte)
+    # Send a byte of data to the Arduino
+    data_byte = 0x41  # Byte value to send
+    ser.write(data_byte)
 
-# Close the serial connection
-ser.close()
+    # Close the serial connection
+    ser.close()
+except Exception as e:
+    print("Serial communication error:", str(e))
+
+    
