a911599d4964fa4bacec4a4c9196b892e57abc8c
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-07-01 19:14:13 -0400

    more debug

--- main_pi.py
+++ main_pi.py
@@ -1,13 +1,25 @@
 import serial
 import time
 
-serial_port = '/dev/serial1' 
+# Define the serial port and baud rate
+serial_port = '/dev/serial1'  # Raspberry Pi's Serial1 port
 baud_rate = 9600
 
-ser = serial.Serial(serial_port, baud_rate, timeout=1)
+try:
+    # Initialize the serial connection
+    ser = serial.Serial(serial_port, baud_rate, timeout=1)
+    print("Serial connection established!")
 
-time.sleep(2)
+    # Wait for the Arduino to reset (if necessary)
+    time.sleep(2)
 
-ser.write('H'.encode())
+    # Send data to Arduino
+    data = 'H'
+    ser.write(data.encode())
+    print("Data sent to Arduino:", data)
 
-ser.close()
+    # Close the serial connection
+    ser.close()
+    print("Serial connection closed.")
+except serial.SerialException as e:
+    print("Serial connection failed:", str(e))
