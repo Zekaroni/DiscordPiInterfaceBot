b78587754b809b10a0e4ecec5243b9d8508bf4b9
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-07-02 00:05:34 -0400

    maybe

--- main_pi.py
+++ main_pi.py
@@ -1,17 +1,21 @@
 import serial
-from time import sleep
+import time
 
 # Configure serial communication
 serial_port = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=1)
 
 # Send data to Arduino
 def send_data(data):
+    if isinstance(data, int):
+        byte_data = data.to_bytes(2, byteorder='big')  # Convert integer to 2-byte representation
+    elif isinstance(data, str):
+        byte_data = data.encode()  # Convert string to byte
     try:
-        serial_port.write(data)
-        print("Data sent successfully:", data)
+        serial_port.write(byte_data)
+        print("Data sent successfully:", byte_data)
     except Exception as e:
         print("Error sending data:", str(e))
-    sleep(0.01)
+
 
 # Example usage
 if __name__ == "__main__":
