9d39fb026a9ad635ffc070a0ae2363daf15c7e38
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-07-02 15:46:36 -0400

    endcode xD

--- main_pi.py
+++ main_pi.py
@@ -6,7 +6,7 @@ serial_port = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=1)
 
 # Send data to Arduino
 def send_data(data):
-    byte_data = data.endcode()
+    byte_data = data.encode()
     try:
         serial_port.write(byte_data)
         print("Data sent successfully:", byte_data)
