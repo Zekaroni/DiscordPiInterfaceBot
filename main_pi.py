b062478bfb4909d602e2d519730f3fb665833b9e
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-07-01 20:10:08 -0400

    fixed but better

--- main_pi.py
+++ main_pi.py
@@ -5,14 +5,15 @@ serial_port = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=1)
 
 # Send data to Arduino
 def send_data(data):
-    if type(data) == bin():
+    if isinstance(data, int):
+        data = bytes([data])  # Convert integer to byte
+    elif isinstance(data, str):
+        data = data.encode()  # Convert string to byte
+    try:
         serial_port.write(data)
-    else:
-        try:
-            serial_port.write(data.encode())
-            print("Data sent successfully:", data)
-        except Exception as e:
-            print("Error sending data:", str(e))
+        print("Data sent successfully:", data)
+    except Exception as e:
+        print("Error sending data:", str(e))
 
 # Example usage
 send_data(0xFF)
