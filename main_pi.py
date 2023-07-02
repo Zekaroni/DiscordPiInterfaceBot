669d4a9d920d3907c815dad1051cf109b07de9a0
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-07-02 16:19:10 -0400

    writing each seperately

--- main_pi.py
+++ main_pi.py
@@ -7,12 +7,12 @@ serial_port = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=1)
 
 def send_data(data):
     try:
-        # Pack the four bytes into a 32-bit value in big-endian format
-        packed_data = struct.pack('<I', data)
-
-        # Send the packed data to the Arduino
-        serial_port.write(packed_data)
-        print("Data sent successfully:", packed_data)
+        # Send the four bytes individually to the Arduino
+        serial_port.write((data >> 24) & 0xFF)
+        serial_port.write((data >> 16) & 0xFF)
+        serial_port.write((data >> 8) & 0xFF)
+        serial_port.write(data & 0xFF)
+        print("Data sent successfully:", hex(data))
     except Exception as e:
         print("Error sending data:", str(e))
     time.sleep(0.01)
