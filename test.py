8f05413bc93255f10290195109d2ba2b2f9ff329
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-07-06 03:38:57 -0400

    added testing

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from serial import Serial
 from struct import pack as struct_pack
+from time import sleep
 
 class SerialOutput:
     def __init__(self,
@@ -20,7 +21,24 @@ class SerialOutput:
             if self._debug:
                 print("Error sending data:", str(e))
 
-SendClient = SerialOutput()
-
 if __name__ == "__main__":
-    SendClient.send_data(0b1010100000000000)
+    SendClient = SerialOutput()
+
+    print("Now sending data with wrong verification:")
+    SendClient.send_data(0b1011101011111111) # Setting motor 1 to max speed, but should fail
+    sleep(2)
+    print("Data was sent, if motor didn't turn on, test passed")
+
+    print("Now turning on motor 1, none others should turn on.")
+    SendClient.send_data(0b1010101011111111) # Max speed on motor 1
+    sleep(2)
+    print("Now turning off motor 1. (NOTE: there should be no humming from the motor)")
+    SendClient.send_data(0b1010101000000000) # Motor 1 off
+
+    print("Now turning on motor 2, none others should turn on.")
+    SendClient.send_data(0b1010110011111111) # Max speed on motor 2
+    sleep(2)
+    print("Now turning off motor 2. (NOTE: there should be no humming from the motor)")
+    SendClient.send_data(0b1010110000000000) # Motor 2 off
+
+    print("Testing has finished")
