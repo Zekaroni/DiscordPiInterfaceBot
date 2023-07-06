3ae475e08d4f59ba3f1496d0739a0e28fcfcaf31
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-07-06 03:40:21 -0400

    updated timesS

--- test.py
+++ test.py
@@ -26,19 +26,22 @@ if __name__ == "__main__":
 
     print("Now sending data with wrong verification:")
     SendClient.send_data(0b1011101011111111) # Setting motor 1 to max speed, but should fail
+    sleep(5)
+    print("Data was sent, if motor didn't turn on, test passed\n\n")
     sleep(2)
-    print("Data was sent, if motor didn't turn on, test passed")
 
     print("Now turning on motor 1, none others should turn on.")
     SendClient.send_data(0b1010101011111111) # Max speed on motor 1
-    sleep(2)
+    sleep(5)
     print("Now turning off motor 1. (NOTE: there should be no humming from the motor)")
     SendClient.send_data(0b1010101000000000) # Motor 1 off
+    sleep(2)
 
     print("Now turning on motor 2, none others should turn on.")
     SendClient.send_data(0b1010110011111111) # Max speed on motor 2
-    sleep(2)
+    sleep(5)
     print("Now turning off motor 2. (NOTE: there should be no humming from the motor)")
     SendClient.send_data(0b1010110000000000) # Motor 2 off
+    sleep(2)
 
     print("Testing has finished")
