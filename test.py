94ca8d01b50272737032b92fb166738c83f1fe96
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-07-06 03:52:59 -0400

    index error lol

--- test.py
+++ test.py
@@ -25,23 +25,23 @@ if __name__ == "__main__":
     SendClient = SerialOutput()
 
     print("Now sending data with wrong verification:")
-    SendClient.send_data(0b1011101011111111) # Setting motor 1 to max speed, but should fail
+    SendClient.send_data(0b1011100011111111) # Setting motor 1 to max speed, but should fail
     sleep(5)
     print("Data was sent, if motor didn't turn on, test passed\n\n")
     sleep(2)
 
     print("Now turning on motor 1, none others should turn on.")
-    SendClient.send_data(0b1010101011111111) # Max speed on motor 1
+    SendClient.send_data(0b1010100011111111) # Max speed on motor 1
     sleep(5)
     print("Now turning off motor 1. (NOTE: there should be no humming from the motor)")
-    SendClient.send_data(0b1010101000000000) # Motor 1 off
+    SendClient.send_data(0b1010100000000000) # Motor 1 off
     sleep(2)
 
     print("Now turning on motor 2, none others should turn on.")
-    SendClient.send_data(0b1010110011111111) # Max speed on motor 2
+    SendClient.send_data(0b1010101011111111) # Max speed on motor 2
     sleep(5)
     print("Now turning off motor 2. (NOTE: there should be no humming from the motor)")
-    SendClient.send_data(0b1010110000000000) # Motor 2 off
+    SendClient.send_data(0b1010101000000000) # Motor 2 off
     sleep(2)
 
     print("Testing has finished")
