b601a952ba6676aa30f5a239c8cbb3b0b1767e2a
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-07-08 14:00:40 -0400

    uncomented test

--- test.py
+++ test.py
@@ -24,49 +24,44 @@ class SerialOutput:
 if __name__ == "__main__":
     SendClient = SerialOutput()
 
-    bits = 0b1010111100000000
-    SendClient.send_data(bits+255)
-    sleep(3)
-    SendClient.send_data(bits)
+    print("Now sending data with wrong verification:")
+    SendClient.send_data(0b1011100011111111) # Setting motor 1 to max speed, but should fail
+    sleep(5)
+    print("Data was sent, if motor didn't turn on, test passed\n\n")
+    sleep(2)
 
-    # print("Now sending data with wrong verification:")
-    # SendClient.send_data(0b1011100011111111) # Setting motor 1 to max speed, but should fail
-    # sleep(5)
-    # print("Data was sent, if motor didn't turn on, test passed\n\n")
-    # sleep(2)
+    print("Now turning on motor 1, none others should turn on.")
+    SendClient.send_data(0b1010100011111111) # Max speed on motor 1
+    sleep(5)
 
-    # print("Now turning on motor 1, none others should turn on.")
-    # SendClient.send_data(0b1010100011111111) # Max speed on motor 1
-    # sleep(5)
+    print("Now turning on motor 2, none others should turn on.")
+    SendClient.send_data(0b1010101011111111) # Max speed on motor 2
+    sleep(5)
 
-    # print("Now turning on motor 2, none others should turn on.")
-    # SendClient.send_data(0b1010101011111111) # Max speed on motor 2
-    # sleep(5)
-
-    # print("Now turning on motor 3, none others should turn on.")
-    # SendClient.send_data(0b1010110011111111) # Max speed on motor 2
-    # sleep(5)
+    print("Now turning on motor 3, none others should turn on.")
+    SendClient.send_data(0b1010110011111111) # Max speed on motor 2
+    sleep(5)
 
 
-    # print("Now turning on motor 4, none others should turn on.")
-    # SendClient.send_data(0b1010111011111111) # Max speed on motor 2
-    # sleep(5)
+    print("Now turning on motor 4, none others should turn on.")
+    SendClient.send_data(0b1010111011111111) # Max speed on motor 2
+    sleep(5)
 
-    # print("Now turning off motor 1. (NOTE: there should be no humming from the motor)")
-    # SendClient.send_data(0b1010100000000000) # Motor 1 off
-    # sleep(2)
+    print("Now turning off motor 1. (NOTE: there should be no humming from the motor)")
+    SendClient.send_data(0b1010100000000000) # Motor 1 off
+    sleep(2)
 
-    # print("Now turning off motor 2. (NOTE: there should be no humming from the motor)")
-    # SendClient.send_data(0b1010101000000000) # Motor 2 off
-    # sleep(2)
+    print("Now turning off motor 2. (NOTE: there should be no humming from the motor)")
+    SendClient.send_data(0b1010101000000000) # Motor 2 off
+    sleep(2)
 
-    # print("Now turning off motor 3. (NOTE: there should be no humming from the motor)")
-    # SendClient.send_data(0b1010110000000000) # Motor 2 off
-    # sleep(2)
+    print("Now turning off motor 3. (NOTE: there should be no humming from the motor)")
+    SendClient.send_data(0b1010110000000000) # Motor 2 off
+    sleep(2)
 
-    # print("Now turning off motor 4. (NOTE: there should be no humming from the motor)")
-    # SendClient.send_data(0b1010111000000000) # Motor 2 off
-    # sleep(2)
+    print("Now turning off motor 4. (NOTE: there should be no humming from the motor)")
+    SendClient.send_data(0b1010111000000000) # Motor 2 off
+    sleep(2)
 
 
-    # print("Testing has finished")
+    print("Testing has finished")
